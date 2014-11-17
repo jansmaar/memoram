3727723f1c69e64e66f54b6d881e17b349080dd4
Jorik <jorik12@hotmail.com>
2014-11-17 10:52:47 +0100

    live upload jpg paperclip js

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -144,3 +144,19 @@ jQuery(document).ready(function($) {
 		preview.html(input.val().replace(/\r\n|\r|\n/g, '<br />'));
 	});
 });
+
+// upload van foto live weergave
+$(document).ready(function(){
+    var preview = $(".upload-preview img");
+    
+    $(".file").change(function(event){
+       var input = $(event.currentTarget);
+       var file = input[0].files[0];
+       var reader = new FileReader();
+       reader.onload = function(e){
+           image_base64 = e.target.result;
+           preview.attr("src", image_base64);
+       };
+       reader.readAsDataURL(file);
+    });
+});
